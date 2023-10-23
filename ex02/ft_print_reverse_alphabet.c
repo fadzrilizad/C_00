b6a5342139a43a3373ce32561920b8a2b6804c0e
Fadzril Izad <32401964+fadzrilizad@users.noreply.github.com>
2023-10-23 21:11:25 +0800

    Init file

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fizad <fizad@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 16:27:58 by fizad             #+#    #+#             */
+/*   Updated: 2023/08/08 18:26:02 by fizad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		write(1, &z, 1);
+		z--;
+	}
+}
+/*
+int main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
+*/
